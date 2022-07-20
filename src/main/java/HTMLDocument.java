01f550bdc6d9799a193bf273a5473b8cdb3a1ba6
James <69613061+jameswright722@users.noreply.github.com>
2022-07-19 18:09:35 -0700

    Update HTMLDocument.java

--- src/main/java/HTMLDocument.java
+++ src/main/java/HTMLDocument.java
@@ -6,26 +6,5 @@ import java.util.HashMap;
 // import java.util.Vector;
 
 public final class HTMLDocument {
-    /**
-     * ItemFactory is a collection of static functions. There is no reason to
-     * instatiate an ItemFactory object.
-     */
-    private HTMLDocument()
-    {
-        // do not allow ItemFactory to be instantiated.
-    }
-
-    /**
-     * This Lookup table contains a listing of all known keywords and the Item
-     * sub-classes to which the correspond.
-     */
-    private static final Map<String, Resource> KNOWN_RESOURCES = new HashMap<String, Resource>() {{
-        // put("StyleSheet", new Stylesheet());
-        // put("JavaScript", new Script());
-        // put("Anchor", new Anchor());
-        put("Image", new Image());
-    }};
-
-
-
+    
 }
