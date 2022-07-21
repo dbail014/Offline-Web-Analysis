3a196b325a3c105647544413381f0fe0eec076de
James <jameswright722@gmail.com>
2022-07-20 21:20:11 -0700

    Resource class.

--- app/src/main/java/edu/odu/cs/cs350/OfflineWebAnalysis/Resource.java
+++ app/src/main/java/edu/odu/cs/cs350/OfflineWebAnalysis/Resource.java
@@ -5,14 +5,12 @@ public abstract class Resource implements Cloneable {
     // data members
     protected String URIpath;
     protected Classification classification;
-    protected int localPageNumber;
 
     // TODO
     // Default Constructor
     public Resource() {
         this.URIpath = "";
         this.classification = Classification.EXTERNAL;
-        this.localPageNumber = 0;
     }
 
     /*
@@ -20,10 +18,9 @@ public abstract class Resource implements Cloneable {
      * 
      * @param _name desired name
      */
-    public Resource(String _URIpath, Classification _classification, int _localPageNumber) {
+    public Resource(String _URIpath, Classification _classification) {
         this.URIpath = _URIpath;
         this.classification = _classification;
-        this.localPageNumber = _localPageNumber;
     }
 
     /*
@@ -61,25 +58,6 @@ public abstract class Resource implements Cloneable {
     public void setClassification(Classification _classification) {
         this.classification = _classification;
     }
-
-    /*
-     * Retrieve URI Path
-     * 
-     * @return current URI Path
-     */
-    public int getLocalPageNumber() {
-        return this.localPageNumber;
-    }
-
-    /*
-     * Update URI Path
-     * 
-     * @param _URIpath replacement URIpath
-     */
-    public void setLocalPageNumber(int _localPageNumber) {
-        this.localPageNumber = _localPageNumber;
-    }
-
     /*
      * Check for logical equivalence.
      * 
