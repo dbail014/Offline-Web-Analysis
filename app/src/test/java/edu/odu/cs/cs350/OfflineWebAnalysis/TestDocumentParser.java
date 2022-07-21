4a3ded3b28292793124001e665573000f9a80246
jwasikye <jwasi001@odu.edu>
2022-07-21 15:21:15 -0400

    added test framework

---
+++ app/src/test/java/edu/odu/cs/cs350/OfflineWebAnalysis/TestDocumentParser.java
@@ -0,0 +1,32 @@
+package edu.odu.cs.cs350.OfflineWebAnalysis;
+
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+class TestDoucmentParser
+{
+
+    @Test
+    void testSetImageVector()
+    {
+        
+    }
+
+    @Test 
+    void testCreateAbsoluteFilePathWindows()
+    {
+
+    }
+
+    // test the constructor
+    @Test
+    void TestDoucmentParser()
+    {
+
+    }
+}
