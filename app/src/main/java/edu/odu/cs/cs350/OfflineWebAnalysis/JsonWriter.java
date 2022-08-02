543a397a79753f340cdb5dc5c5e882507b54ff9e
Brian Clark <aclar074@odu.edu>
2022-08-02 16:57:40 -0400

    JSON import issues

--- app/src/main/java/edu/odu/cs/cs350/OfflineWebAnalysis/JsonWriter.java
+++ app/src/main/java/edu/odu/cs/cs350/OfflineWebAnalysis/JsonWriter.java
@@ -1,8 +1,15 @@
 package edu.odu.cs.cs350.OfflineWebAnalysis;
 
+import java.io.FileWriter;
+import java.io.IOException;
 
-public class JsonWriter {
+// not importing for some reason?
+import com.cedarsoftware.util.io.JsonObject;
+import com.cedarsoftware.util.io.JsonWriter;
+import com.cedarsoftware.util.io.JsonReader;
+
+
+public class JsonWriter extends ReportWriter {
     
-
 
 }
